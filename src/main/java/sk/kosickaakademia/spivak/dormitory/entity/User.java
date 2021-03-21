a8420222bf393f5492c59296f514e02683f20f1b
illiaspivak <illia.spivak@kosickaakademia.sk>
2021-03-21 23:33:09 +0100

    Class User

---
+++ src/main/java/sk/kosickaakademia/spivak/dormitory/entity/User.java
@@ -0,0 +1,50 @@
+package sk.kosickaakademia.spivak.dormitory.entity;
+
+import java.util.Date;
+
+public class User {
+    private int id;
+    private String fname ;
+    private String lname ;
+    private Date dob;
+    private String country;
+    private String city;
+    private String room;
+
+    public User(String fname, String lname, Date dob, String country, String city, String room) {
+        this.fname = fname;
+        this.lname = lname;
+        this.dob = dob;
+        this.country = country;
+        this.city = city;
+        this.room = room;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getRoom() {
+        return room;
+    }
+}
