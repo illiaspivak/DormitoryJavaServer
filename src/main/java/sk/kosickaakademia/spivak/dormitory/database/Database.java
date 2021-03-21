1d63cddb7ec282e21a161321d6ba9450c00d7e93
illiaspivak <illia.spivak@kosickaakademia.sk>
2021-03-21 23:19:30 +0100

    Change methods getConnection and closeConnection

--- src/main/java/sk/kosickaakademia/spivak/dormitory/database/Database.java
+++ src/main/java/sk/kosickaakademia/spivak/dormitory/database/Database.java
@@ -10,6 +10,10 @@ import java.sql.SQLException;
 public class Database {
     Log log = new Log();
 
+    /**
+     * Connecting to the database
+     * @return
+     */
     public Connection getConnection() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -17,17 +21,23 @@ public class Database {
             log.print("Connection is successful");
             return connection;
         }catch (Exception ex) {
+            log.error("No connection to the database");
             log.error(ex.toString());
         }
         return null;
     }
 
+    /**
+     * Close the database connection
+     * @param connection
+     */
     public void closeConnection(Connection connection)  {
         if(connection!=null) {
             try {
                 connection.close();
                 log.print("Connection closed");
             }catch(SQLException e){
+                log.error("Failed to close database connection");
                 log.error(e.toString());
             }
         }
